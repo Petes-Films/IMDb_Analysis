74f6e4df258cbcc4c40dd77479718a02b280a183
Peter <peterwynhowells@gmail.com>
2026-09-19 09:46:58 +0100

    Updating analysis for clearer house styles to use in the charts

--- R/globals.R
+++ R/globals.R
@@ -21,7 +21,21 @@ library(purrr)
 # knitr defaults (used by the report)
 knitr::opts_chunk$set(echo = T, fig.align = "center", warning = F, message = F)
 
-# Unified ggplot theme
+# Standard default Variables
+Defaults <- list(
+  min_films = 5,
+  min_genre_films = 100,
+  min_votes = 100,
+  score_limits = c(1, 10),
+  ex_dif = 1,
+  no_IMDb_rating = 1
+)
+
+# decade converter function
+to_decade <- function(year) floor(year / 10) * 10
+
+# House Style for more consistent formatting of tables and charts
+# ggplot theme
 theme_set(
   theme_classic() +
     theme(
@@ -29,12 +43,14 @@ theme_set(
       plot.subtitle = element_text(size = 14, face = "bold", hjust = 0.5),
       axis.line = element_blank(),
       axis.ticks = element_blank(),
+      #    axis.title = element_blank(),
       legend.title=element_blank()
     )
 )
 
+# second theme for blank titles and angled labelling?
 
-# gt theme factory
+# gt table theme
 my_gt_theme <- function(gt_tbl) {
   n_rows <- nrow(gt_tbl$`_data`)
   gt_tbl |>
@@ -56,15 +72,32 @@ my_gt_theme <- function(gt_tbl) {
     )
 }
 
-# Defaults
-Defaults <- list(
-  min_films = 5,
-  min_genre_films = 100,
-  min_votes = 100,
-  score_limits = c(1, 10),
-  ex_dif = 1
+# Colour schemes for charts
+# Main one colour for graphs
+base_col <- "darkgreen"
+
+# Colour gradien tstyle for variation from norm
+col_change <- list(
+  negative = "tomato",
+  neutral  = "grey85",
+  positive = "steelblue"
 )
 
+# Colour scale options for ggplot with additional functionality
+scale_change <- function(midpoint = 0, col_limits = NULL) {
+  scale_colour_gradient2(
+    low = col_change$negative,
+    mid = col_change$neutral,
+    high = col_change$positive,
+    midpoint = midpoint,
+    limits = col_limits, # check I haven't messed this up!
+    guide = "none"
+  )
+}
 
-to_decade <- function(year) floor(year / 10) * 10
+# Reference lines for chart style
+ref_col <- "grey50" # or grey 60
+ref_line <- "dashed"
 
+# Normal chart transparency level
+ref_alpha <- 0.7
